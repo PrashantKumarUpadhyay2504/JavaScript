0f6be2849c93c7d95a600de6f8b544d34bb6e5ea
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-08-03 13:20:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Ways to printing into the javascript
+console.log("hello World");
+// alert("Hello, World!!");
+// document.write("This is Writing");
+
+// Javascript console API
+// console.log("Hello , World!");
+// console.warn("This is a Warning ");
+// console.error("This is an Error");
+
+// JavaScript Variables:
+
+var number1= 45;
+var number2=34;
+
+console.log(number1 + number2);
+
